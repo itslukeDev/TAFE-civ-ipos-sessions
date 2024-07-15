9f5d8340f987d7cb15d5396341c32beb44fa4d90
lukedev <82327125+itslukeDev@users.noreply.github.com>
2024-07-15 13:00:22 +0800

    feat(bottles of beer): initial solution

--- week1/bottles_of_beer/bottles_of_beer.py
+++ week1/bottles_of_beer/bottles_of_beer.py
@@ -1,1 +1,20 @@
+bottles = 99
 
+
+def bottlePlural(count: int):
+    plural = f"{'' if bottles == 1 else 's'}"
+    # 1 bottle, 99 bottles
+    return f"{count} bottle{plural}"
+
+
+def takeOneDown():
+    global bottles
+    bottles -= 1
+    print("Take one down, pass it around!")
+
+
+while bottles > 0:
+    print(f"{bottlePlural(bottles)} of beer on the wall!")
+    print(f"{bottlePlural(bottles)} of beer!")
+    takeOneDown()
+    print(f"{bottlePlural(bottles)} of beer on the wall!\n")
