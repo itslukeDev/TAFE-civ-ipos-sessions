3f36b54603d9ea0cb69cdc4f37f17d86f7688541
Luke L <82327125+itslukeDev@users.noreply.github.com>
2024-07-29 23:34:12 +0800

    refactor: switch to using pardir

--- week3/in-class-demos/workshop_part3/tests/setup.py
+++ week3/in-class-demos/workshop_part3/tests/setup.py
@@ -3,5 +3,7 @@ import os
 
 
 def setup_path():
-    module_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+    module_path = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.path.pardir)
+    )
     sys.path.insert(0, module_path)
