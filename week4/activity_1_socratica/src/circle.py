d69c888fc8c04bf6e9b012494fa91ec4c4516157
lukedev <82327125+itslukeDev@users.noreply.github.com>
2024-08-05 11:20:29 +0800

    tests: uncomment and play with unittests

--- week4/activity_1_socratica/src/circle.py
+++ week4/activity_1_socratica/src/circle.py
@@ -1,11 +1,13 @@
 """Demonstrates Unit Testing Concepts shown in https://www.youtube.com/watch?v=1Lfv5tUGsn8
 We use an example of a function that calculates the area of a circle and show that it fails for certain input values.
 Given this how do we scope what input values we want and how do we document and test that the function does all that we want it to do"""
+
 from math import pi
 
 
 def circle_area(r):
-    # after running unit tests uncomment these lines then progress to adding in the snippet
+    if type(r) not in [int, float]:
+        raise TypeError("The radius must be a non-negative real number")
     if r < 0:
         raise ValueError("Radius cannot be negative.")
 
@@ -15,9 +17,9 @@ def circle_area(r):
 # Will this function work for all values of r? Clearly not. An easy way to demonstrate this is to create a list with
 # a set of values we want to test and then call our function with each of those values using a for loop like so: j in
 # python is the same as i (imaginary) in maths
-if __name__ == '__main__':
+if __name__ == "__main__":
     radii = [2, 0, -3, 2 + 5j, True, "radius"]
-    message = 'Area of circles with r = {radius} is {area}'
+    message = "Area of circles with r = {radius} is {area}"
     for r in radii:
         A = circle_area(r)
         # format is a function we haven't seen before that fills that values in {} braces with the values of the
--- week4/activity_1_socratica/tests/test_circle.py
+++ week4/activity_1_socratica/tests/test_circle.py
@@ -3,8 +3,7 @@
 import unittest  # unit testing framework
 from math import pi
 
-from src.circle import \
-    circle_area  # importing the function we wrote in the other file
+from src.circle import circle_area
 
 
 class TestCircleArea(unittest.TestCase):
@@ -13,13 +12,13 @@ class TestCircleArea(unittest.TestCase):
         # assertAlmostEqual checks that results are the same to 7 decimal points
         self.assertAlmostEqual(circle_area(1), pi)
         self.assertAlmostEqual(circle_area(0), 0)
-        self.assertAlmostEqual(circle_area(2.1), pi * 2.1 ** 2)
+        self.assertAlmostEqual(circle_area(2.1), pi * 2.1**2)
 
     def test_values(self):
         self.assertRaises(ValueError, circle_area, -2)
 
     # uncomment these at end of video and add the snippet in snippet.py to circle.py to pass these tests
-    # def test_types(self):
-    #     self.assertRaises(TypeError, circle_area, 3 + 5j)
-    #     self.assertRaises(TypeError, circle_area, True)
-    #     self.assertRaises(TypeError, circle_area, 'radius')
+    def test_types(self):
+        self.assertRaises(TypeError, circle_area, 3 + 5j)
+        self.assertRaises(TypeError, circle_area, True)
+        self.assertRaises(TypeError, circle_area, "radius")
